47ddb1c6b4886d9db4f39aa992f85a63d3af9cb6
Chizzo Cheese <chizzo84@gmail.com>
2019-01-31 16:47:11 +0200

    Readme and coding

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title></title>
+  <title>Facebook Developer Circle: Lusaka Code Challenge</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -22,7 +22,40 @@
   <![endif]-->
 
   <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+	<p>Input: 3[abc]4[ab]c</p>
+	<?php
+		$compressed_1 = "3[abc]4[ab]c";
+		function decompress($compressed_string)
+		{
+			$result = "";
+			while (strpos($compressed_string, "[") !== false)
+			{
+				$start = strrpos($compressed_string, '[');
+				$end = strpos($compressed_string, ']', $start);
+				$multiplier = substr($compressed_string, ($start - 1), 1);
+				$section = substr($compressed_string, ($start + 1), ($end - $start) - 1);
+				echo $section;
+				echo "<br>";
+				$result = str_repeat($section, (int)$multiplier) . $result;
+				$compressed_string = substr_replace($compressed_string, "", ($start - 2), strlen($section) + 3);
+				echo $compressed_string;
+				echo "<br>";
+			}
+			
+			return $result;
+		}
+		echo decompress($compressed_1);
+	?>
+	<p>Input: 10[a]</p>
+	<?php
+		$compressed_2 = "9[a]";
+		echo decompress($compressed_2);
+	?>
+	<p>Input: 2[3[a]b]</p>
+	<?php
+		$compressed_3 = "2[3[a]b]";
+		echo decompress($compressed_2);
+	?>
   <script src="js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
@@ -30,11 +63,12 @@
   <script src="js/main.js"></script>
 
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <!--
   <script>
     window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
     ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
   </script>
   <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+  -->
 </body>
-
 </html>
